d232d60b350e3731e12f33ebbc515ef6a6a0e632
Cameron Rollheiser <icirellik@gmail.com>
2018-01-09 00:56:50 -0500

    Write to File
    Add basic support for writing games to a file.

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 const scraper = require('./src/scraper');
 
 const gameBrowseRoot = 'https://boardgamegeek.com/browse/boardgame';
+const output = 'games.txt';
 
 (async () => {
-    const browser = await scraper.createBrowser();
+  const fd = fs.openSync(output, 'a');
+  fs.ftruncateSync(fd);
 
-    // Attach SIGINT handler with browser cleanup.
-    process.on('SIGINT', async () => {
-      console.log("Interrupted exiting.");
-      await scraper.closeBrowser(browser);
-      process.exit();
-    });
+  const browser = await scraper.createBrowser();
 
-    const page = await scraper.createPage(browser);
-
-    let bookmark = gameBrowseRoot;
-    let fullGames = [];
-    while (!!bookmark) {
-      const { games, nextUrl, success } = await scraper.gameList(page, bookmark);
-      if (!success) {
-        continue;
-      }
-      bookmark = nextUrl;
-
-      // Get all game details
-      let count = 1;
-      for (let game of games) {
-        console.log(`game details progress - ${count} of ${games.length} (${fullGames.length + 1})`);
-        let fetchDetails = true;
-        while (fetchDetails) {
-          const { gameDetails, success, href } = await scraper.gameDetails(page, game);
-          if (success) {
-            fetchDetails = false;
-            fullGames.push(gameDetails);
-          }
-        }
-        count++;
-      }
-      console.log(`Games Loaded ${fullGames.length} - ${bookmark}`)
-    }
-
+  // Attach SIGINT handler with browser cleanup.
+  process.on('SIGINT', async () => {
+    console.log("Interrupted exiting.");
     await scraper.closeBrowser(browser);
+    process.exit();
+  });
+
+  const page = await scraper.createPage(browser);
+
+  let bookmark = gameBrowseRoot;
+  let fullGames = 1;
+  while (!!bookmark) {
+    const { games, nextUrl, success } = await scraper.gameList(page, bookmark);
+    if (!success) {
+      continue;
+    }
+    bookmark = nextUrl;
+
+    // Get all game details
+    let count = 1;
+    for (let game of games) {
+      console.log(`game details progress - ${count} of ${games.length} (${fullGames.length})`);
+      let fetchDetails = true;
+      while (fetchDetails) {
+        const { gameDetails, success, href } = await scraper.gameDetails(page, game);
+        if (success) {
+          fetchDetails = false;
+          fs.writeSync(fd, JSON.stringify(gameDetails) + '\n');
+          fullGames++;
+        }
+      }
+      count++;
+    }
+    console.log(`Games Loaded ${fullGames.length} - ${bookmark}`)
+  }
+
+  await scraper.closeBrowser(browser);
+  fs.closeSync(fd);
 })()
 .catch(err => {
   console.log(err);
